1f4e2b18329428881d182574a828ee20b208a95b
vandana-engineer <vandanacharabudla21@gmail.com>
2026-06-14 10:19:26 -0400

    Create etl.py

---
+++ src/etl.py
@@ -0,0 +1,13 @@
+def extract():
+    print("Extracting data")
+
+def transform():
+    print("Transforming data")
+
+def load():
+    print("Loading data")
+
+if __name__ == "__main__":
+    extract()
+    transform()
+    load()
